d65920c599b6f5802a6b7dd803d4255d31f998af
devravindersingh <ravindersngh402@outlook.com>
2021-01-24 19:41:47 +0530

    Initial commit

---
+++ microservice/product-composite-service/src/test/java/com/ravinder/singh/composite/product/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravinder.singh.composite.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
