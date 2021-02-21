9eb88187e17de1cc922b97fba81f96ae5d39b328
devravindersingh <ravindersngh402@outlook.com>
2021-02-21 11:11:42 +0530

    added core services implementations
    ProductServiceImpl, ReviewServiceImpl and RecommendationServiceImpl.
    Application.yml also updated for all core services.

--- microservice/recommendation-service/src/main/java/com/ravinder/singh/core/recommendation/RecommendationServiceApplication.java
+++ microservice/recommendation-service/src/main/java/com/ravinder/singh/core/recommendation/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package com.ravinder.singh.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ravinder.singh")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
